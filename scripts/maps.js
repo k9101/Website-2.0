3bc4578edad3b5c3d94ae8e8c2ef41c98b6a8d82
k9101 <kris9101@gmail.com>
2015-05-21 18:20:27 -0400

    server script updates

--- scripts/maps.js
+++ scripts/maps.js
@@ -1,41 +1,66 @@
 
 
+var lat;
+var long;
+var message;
+
+function getMarkerData(){
+  var getter = $.ajax({
+                  url: 'data/location.json',
+                  type: 'GET',
+                  success: function(data){
+
+                    lat = parseFloat(data.lat);
+                    long = parseFloat(data.long);
+                    message = data.message;
+                  }
+                });
+
+  return getter;
+}
+
 //Load map, center on location
 function initialize() {
 
-  var latLng = new google.maps.LatLng(43.472286, -80.544858);
+  var getter = getMarkerData();
 
-  var mapOptions = {
-    center: latLng,
-    zoom: 16
-  };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  $.when(getter).done(function(){
+    var latLng = new google.maps.LatLng(lat, long);
 
-  var contentString = '<div id="content" class="mapContent">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading"><b>Where is Kris?</b></h1>'+
-        '<div id="bodyContent">'+
-          "</br><p>Kris is currently completing his 2B term for his BBA/BCS Double Degree</br><!--<a href='#Crawford'>See More</a>--></p>" +
-        '</div>'+
-        '</div>';
+    var mapOptions = {
+      center: latLng,
+      zoom: 16
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
 
-  var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
+    var contentString = '<div id="content" class="mapContent">'+
+          '<div id="siteNotice">'+
+          '</div>'+
+          '<h1 id="firstHeading" class="firstHeading"><b>Where is Kris?</b></h1>'+
+          '<div id="bodyContent">'+
+            "</br><p>" + message + "</p></br>" +//"Kris is currently completing his 2B term for his BBA/BCS Double Degree</br><!--<a href='#Crawford'>See More</a>--></p>" +
+          '</div>'+
+          '</div>';
 
-    var marker = new google.maps.Marker({//build marker
-        position: latLng,
-        map: map,
-        title: 'Where is Kris?'
-    });
+    var infowindow = new google.maps.InfoWindow({
+          content: contentString
+      });
 
-    infowindow.open(map,marker);//open initially
+      var marker = new google.maps.Marker({//build marker
+          position: latLng,
+          map: map,
+          title: 'Where is Kris?'
+      });
 
-    google.maps.event.addListener(marker, 'click', function() {
-       infowindow.open(map,marker);
-    });
+      infowindow.open(map,marker);//open initially
+
+      google.maps.event.addListener(marker, 'click', function() {
+         infowindow.open(map,marker);
+      });
+
+  });
+
 
 }
 google.maps.event.addDomListener(window, 'load', initialize);
--- server.cgi
+++ server.cgi
@@ -4,22 +4,28 @@
 #enable debugging
 import cgitb
 import json
+import urlparse
+import os
+
 cgitb.enable()
 
-print "Content-Type: text/plain;charset=utf-8"
-print
 
-print "Hello World!"
+url = os.environ['HTTP_HOST']
+uri = os.environ['REQUEST_URI']
 
+parsed = urlparse.urlparse(url+uri)
+params = urlparse.parse_qs(parsed.query)
 
 
 try:
   f = open("data/location.json", 'w')
 
-  locationJSON = {'lat':66, 'long':67, 'message': 'I am Kris!'}
+  locationJSON = {'lat':params["Lat"][0], 'long':params["Long"][0], 'message': params["Message"][0]}#, 'params': params, 'url': url, 'uri': uri}
 
   json.dump(locationJSON, f)
 
   f.close()
+
+  print HttpRes
 except:
   print 'failed!'
