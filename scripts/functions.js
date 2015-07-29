f1fd9e722e6837a2cbc81d180c42055457a58475
k9101 <kris9101@gmail.com>
2015-07-28 23:08:09 -0400

    Simplified File Structure and Javascript to reflect the switch to a single JSON file

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,155 +1,98 @@
 $(document).ready(function(){
 
   var jumboHeight = $('#jumboContainer').outerHeight();
-  var jSkills;
-  var jWork;
-  var jEdu;
 
-function daysInMonth(month,year) {
-    return new Date(year, month, 0).getDate();
-}
+  function daysInMonth(month,year) {
+      return new Date(year, month, 0).getDate();
+  }
 
 
-//calculate degree progress - 15 terms in total
-function degreeProgress(){
-//  var width = 100/15 * numberOfTerms + "%";
-  var date = new Date();
-  var yearSinceStart = date.getFullYear() - 2014; //get full years
-  var month = date.getMonth() + 1;
-  var days = date.getDate();
-  var daysPercent = days/daysInMonth(month, date.getFullYear());
-  var numberOfTerms = 3 * yearSinceStart + (month + daysPercent)/4 + 1; // account for Fall 2013
-  var width = 100/15 * numberOfTerms + "%";
+  //calculate degree progress - 15 terms in total
+  function degreeProgress(){
+  //  var width = 100/15 * numberOfTerms + "%";
+    var date = new Date();
+    var yearSinceStart = date.getFullYear() - 2014; //get full years
+    var month = date.getMonth() + 1;
+    var days = date.getDate();
+    var daysPercent = days/daysInMonth(month, date.getFullYear());
+    var numberOfTerms = 3 * yearSinceStart + (month + daysPercent)/4 + 1; // account for Fall 2013
+    var width = 100/15 * numberOfTerms + "%";
 
-  $('#bar').animate({
-    width: width
-  }, 4000);
+    $('#bar').animate({
+      width: width
+    }, 4000);
 
-//  $('#bar').css('width', 100/15 * numberOfTerms + "%");
-}
+  //  $('#bar').css('width', 100/15 * numberOfTerms + "%");
+  }
 
-function generateWorkExperienceMediaObject(imageURL, title, time, skills, id){
+  function generateCard(imageURL, title, time, skills, id){
 
-  var skillsList = "";
+    var skillsList = "";
 
-  $.each(skills, function(index,  value){
-    skillsList += "<li>" + value + "</li>";
-  });
+    $.each(skills, function(index,  value){
+      skillsList += "<li>" + value + "</li>";
+    });
 
-  var media = "<div id='" + id + "' class='workMedia dropshadow flex'>" +
-                "<div class='workMediaImage'>" +
-                  "<img src='" + imageURL + "'/>" +
-                "</div>" +
-                "<div class='workMediaContent'>" +
-                  "<div class='workMediaHeader'>" + title + "<span>" + time + "</span></div>" +
-                  "<div class='workMediaList'>" +
-                    "<ul>" + skillsList + "</ul>" +
+    var media = "<div id='" + id + "' class='workMedia dropshadow flex'>" +
+                  "<div class='workMediaImage'>" +
+                    "<img src='" + imageURL + "'/>" +
                   "</div>" +
-                "</div>" +
-                "</div>";
-
-  return media;
-}
-
-function generateWorkExperienceSmallMediaObject(imageURL, title, time, skills){
-
-  var skillsList = "";
-
-  $.each(skills, function(index,  value){
-    skillsList += "<li>" + value + "</li>";
-  });
-
-  var media = "<div class='workMedia flex dropshadow workMedia-small'>" +
-                "<div class='workMediaImage workMediaImage-small'>" +
-                  "<img src='" + imageURL + "'/>" +
-                "</div>" +
-                "<div class='workMediaContent'>" +
-                  "<div class='workMediaHeader'>" + title + "<span>" + time + "</span></div>" +
-                  "<div class='workMediaList'>" +
-                    "<ul>" + skillsList + "</ul>" +
+                  "<div class='workMediaContent'>" +
+                    "<div class='workMediaHeader'>" + title + "<span>" + time + "</span></div>" +
+                    "<div class='workMediaList'>" +
+                      "<ul>" + skillsList + "</ul>" +
+                    "</div>" +
                   "</div>" +
-                "</div>" +
-                "</div>";
+                  "</div>";
 
-  return media;
-}
+    return media;
+  }
 
-// //populate work experience media objects
-//   $.ajax({
-//     url: 'data/workExperience.json',
-//     type: "GET",
-//     success: function(data) {
-//
-//   //    var jData = JSON.parse(data);
-//
-//       $.each(data.WorkExperience, function(index, value){
-//         var media = generateWorkExperienceMediaObject(value.logo, value.title, value.time, value.skills, value.id);
-//
-//         $('.experienceContainer').append(media);
-//       });
-//
-//     }
-//   });
-//
-//
-// //populate education media objects
-//   $.ajax({
-//     url: 'data/education.json',
-//     type: 'GET',
-//     success: function(data){
-//
-//     //  var jData = JSON.parse(data);
-//
-//       $.each(data.Education, function(index, value){
-//
-//         var media = generateWorkExperienceMediaObject(value.logo, value.program, value.time, value.notes, "");
-//
-//         $('.educationContainer').append(media);
-//       });
-//     }
-//   });
-//
-//   $.ajax({
-//     url: 'data/skills.json',
-//     type: 'GET',
-//     success: function(data){
-//
-//       $.each(data.skills, function(index, value){
-//
-//         var media = generateWorkExperienceSmallMediaObject(value.img, value.skill, "", value.points);
-//
-//         $('.skillsContainer').append(media);
-//       });
-//     }
-//   });
+  //Generates
+  function generateSmallCard(imageURL, title, time, skills){
+
+    var skillsList = "";
+
+    $.each(skills, function(index,  value){
+      skillsList += "<li>" + value + "</li>";
+    });
+
+    var media = "<div class='workMedia flex dropshadow workMedia-small'>" +
+                  "<div class='workMediaImage workMediaImage-small'>" +
+                    "<img src='" + imageURL + "'/>" +
+                  "</div>" +
+                  "<div class='workMediaContent'>" +
+                    "<div class='workMediaHeader'>" + title + "<span>" + time + "</span></div>" +
+                    "<div class='workMediaList'>" +
+                      "<ul>" + skillsList + "</ul>" +
+                    "</div>" +
+                  "</div>" +
+                  "</div>";
+
+    return media;
+  }
 
   $.ajax({
       url: "data/data.json",
       type: 'GET',
       success: function(data){
 
-        // jSkills = data.skills;
-        // jWork = data.WorkExperience;
-        // jEdu = data.Education;
-        var jData = JSON.parse(data);
-        console.log(jData);
-
-        $.each(jData.WorkExperience, function(index, value){
-          var media = generateWorkExperienceMediaObject(value.logo, value.title, value.time, value.skills, value.id);
+        $.each(data.WorkExperience, function(index, value){
+          var media = generateCard(value.logo, value.title, value.time, value.skills, value.id);
 
           $('.experienceContainer').append(media);
         });
 
-        $.each(jData.Education, function(index, value){
+        $.each(data.Education, function(index, value){
 
-          var media = generateWorkExperienceMediaObject(value.logo, value.program, value.time, value.notes, "");
+          var media = generateCard(value.logo, value.program, value.time, value.notes, "");
 
           $('.educationContainer').append(media);
         });
 
-        $.each(jData.skills, function(index, value){
+        $.each(data.skills, function(index, value){
 
-          var media = generateWorkExperienceSmallMediaObject(value.img, value.skill, "", value.points);
+          var media = generateSmallCard(value.img, value.skill, "", value.points);
 
           $('.skillsContainer').append(media);
         });
